8f17dc9404b25ae0c5851aa33a95685aa74ca235
Yusuke INOUE <>
2019-10-11 16:54:54 +0900

    make select function

--- KLab/ScrimPay/models.py
+++ KLab/ScrimPay/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core import validators
 
 # Create your models here.
 class Main(models.Model):
@@ -11,6 +12,8 @@ class Main(models.Model):
 
     # サービスID : Char型で、外部キー
     service_id = models.CharField(max_length=30)
+
+
 
 class User(models.Model):
     # ユーザーID : Char型で、主キー
@@ -37,3 +40,37 @@ class Service(models.Model):
 
     # 月額料金 : Integer型
     fee_per_month = models.IntegerField()
+
+"""
+class Item(models.Model):
+    service_name1 : models.BooleanField(verbose_name="Amazon Prime(月額)")
+    service_name2 : models.BooleanField(verbose_name="Amazon Prime(年額)")
+    service_name3 : models.BooleanField(verbose_name="Amazon Prime Prime student(月額)")
+    service_name4 : models.BooleanField(verbose_name="Amazon Prime Prime student(年額)")
+    service_name5 : models.BooleanField(verbose_name="Hulu(月額)")
+    service_name6 : models.BooleanField(verbose_name="Netflix(ベーシック)")
+    service_name7 : models.BooleanField(verbose_name="Netflix(スタンダード)")
+    service_name8 : models.BooleanField(verbose_name="Netflix(プレミアム)")
+    service_name9 : models.BooleanField(verbose_name="U-NEXT")
+    service_name10 : models.BooleanField(verbose_name="dTV")
+    service_name11 : models.BooleanField(verbose_name="dアニメストア")
+    service_name12 : models.BooleanField(verbose_name="Youtube Premioum(一般)")
+    service_name13 : models.BooleanField(verbose_name="Youtube Premioum(iOS版)")
+    service_name14 : models.BooleanField(verbose_name="Youtube Premioum(ファミリー)")
+    service_name15 : models.BooleanField(verbose_name="Youtube Premioum(学割)")
+    service_name16 : models.BooleanField(verbose_name="DAZN(一般)")
+    service_name17 : models.BooleanField(verbose_name="DAZN(DAZN for docomo)")
+    service_name18 : models.BooleanField(verbose_name="DAZN(スマホナビ割)")
+    service_name19 : models.BooleanField(verbose_name="Abemaプレミアム")
+    service_name20 : models.BooleanField(verbose_name="ビデオパス")
+    service_name21 : models.BooleanField(verbose_name="FOD(FODプレミアム)")
+    service_name22 : models.BooleanField(verbose_name="FOD(ネクストsmart)")
+    service_name23 : models.BooleanField(verbose_name="FOD(ワンツーsmart)")
+    service_name24 : models.BooleanField(verbose_name="FOD(ワンツーネクストsmart)")
+    service_name25 : models.BooleanField(verbose_name="Paravi")
+    service_name26 : models.BooleanField(verbose_name="TSUTAYA TV(見放題プラン)")
+    service_name27 : models.BooleanField(verbose_name="TSUTAYA TV(動画見放題+定額レンタル8)")
+    service_name28 : models.BooleanField(verbose_name="ビデオマーケット(プレミアムコース)")
+    service_name29 : models.BooleanField(verbose_name="ビデオマーケット(プレミアム&見放題コース)")
+                    #service_name10 : models.BooleanField
+"""
--- KLab/ScrimPay/views.py
+++ KLab/ScrimPay/views.py
@@ -1,3 +1,76 @@
+"""
+
 from django.shortcuts import render
+from django.urls import reverse_lazy
+#from .forms import ItemForm
+#from .models import Service
+#from .django.views.generic.edit import CreateView
+from .models import Select_Service
 
 # Create your views here.
+
+#def post_new(request):
+#    form = ItemForm()
+#    return render(request, 'blog/post_edit.html', {'form': form})
+"""
+
+
+from django.views.generic.edit import CreateView
+from .models import User 
+from .models import Service
+from .models import Main
+from django.http import HttpResponse
+from django.views.generic import TemplateView
+from django.shortcuts import render
+import logging
+
+logger=logging.getLogger(__name__)
+
+
+
+class ItemCreateView(TemplateView):
+    template_name = 'ScrimPay/index.html'
+    model = Main
+    def post(self, request, *args, **kwargs):
+        some_var = request.POST.getlist('checks[]')
+        print(str(some_var))
+        number = len(some_var)
+
+        data = Service.objects.all()
+        sum = 0
+        for w in some_var:
+            #print(w)
+            #print(data[int(w)-1].fee_per_month)
+            #print(data[int(w)-1])
+            sum = int(data[int(w)-1].fee_per_month) + sum
+        
+        print("sum = "+str(sum))
+
+        for w in some_var:
+            print(data[int(w)-1].service_id + " : " + str(int(data[int(w)-1].fee_per_month)/sum*100) + "%")
+
+        #引数としてuser_id(A001)があった場合
+        for w in some_var:
+            main_data = Main(user_id = 'A001',service_id = str(data[int(w)-1].service_id))
+            print(main_data.user_id)
+            print(main_data.service_id)
+            main_data.save()
+
+        return HttpResponse('')
+
+
+
+        
+
+'''
+def hello(request):
+    data = Service.objects.all()
+    d = {
+        'val':data,
+    }
+    return render(request, 'ScrimPay/index.html',d)
+
+
+def calcmain(request):
+    return HttpResponse('<h1>{{some_var}}</h1>')
+'''
